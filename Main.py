cbcc0d9185276a178f3e80122ed8468d526a3edd
josiah705 <josiahmwilson@gmail.com>
2025-06-10 15:36:28 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+
+
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
